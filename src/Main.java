966c6d79339e848fdef2397705ba653f556de318
Burov Dmitry <BurovDA@list.ru>
2023-07-27 11:56:36 +0300

    Change commit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         BmiService service = new BmiService();
-        int weight = 120; // Вес в килограммых
+        int weight = 120; // Вес в килограммах
         double growth = 1.75; // Рост в метрах
         int bmi = service.index(weight, growth);
         System.out.println("Индекс массы тела составляет " + bmi);
